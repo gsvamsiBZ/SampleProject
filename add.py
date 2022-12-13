5b78081e9850b5915351943bfee2202905928fac
Siddhu <siddharthar938@gmail.com>
2022-12-13 11:27:08 +0530

    grammatic issue resolved

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
 print("Hello siddhartha! This is Add.py in  main/branch1")
-# Reading 2 number and printing their sum
+# Reading 2 numbers and printing their sum
 a,b=map(int,input().split())
 print(a+b)
