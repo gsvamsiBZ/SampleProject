30f7d800fb64d9ec440e0c55a09d81436ec4cf90
Karthik-184 <patelkarthikreddy@gmail.com>
2022-12-15 16:43:36 +0530

    userModel updated

--- backend/db/models/userModel.js
+++ backend/db/models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 var truecallerUserSchema = new Schema({
     phone                             : { type: String, required: true, unique : true},
-    name                              : { type: String},
+    name                              : { type: String, required: true},
     location                          : { type: String},
     email                             : { type: String},
     updated_by                        : { type:String, default: require('os').hostname()},
