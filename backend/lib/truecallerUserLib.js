83fdb21ec3a3901e94f0c3faced42eaceccf39c7
Siddhu <siddharthar938@gmail.com>
2022-12-16 11:16:48 +0530

    Adding getrecords

--- backend/lib/truecallerUserLib.js
+++ backend/lib/truecallerUserLib.js
@@ -2,3 +2,27 @@
 // delete function
 // get all records function
 // get 1 record function
+const truecallermodel=require("../db/models/truecallerUserModel")
+const asyncDblib= require("./asyncDbLib")
+module.exports.getAllRecords=async(req,res)=>{
+    try{
+             const allrecords=await asyncDblib.getAllDocumentsWithFilter(truecallermodel,{})
+             console.log("getting all records ",allrecords)
+             res.json(allrecords)
+    }
+    catch(error){
+        res.status(500).json(error);
+    }
+}
+module.exports.getRecordByNumber=async(req,res)=>{
+    try{
+        let phonenumber=req.query.phone 
+        let filter={phone:phonenumber}
+        const data=await asyncDblib.getOneDocumentByFilter(truecallermodel,filter);
+        console.log("getting records with phone filter of ",filter,"and the data is ",data);
+        res.json(data)
+    }
+    catch(error){
+         res.status(500).json(error);
+    }
+}
