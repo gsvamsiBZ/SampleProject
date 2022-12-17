329fea78ee5e03252b5e12b29a687c9b5ad55fcd
Siddhu <siddharthar938@gmail.com>
2022-12-17 16:13:22 +0530

    updated camelcase

--- backend/routes/allRoutes.js
+++ backend/routes/allRoutes.js
@@ -3,8 +3,8 @@ const truecallerUserLib = require('../lib/truecallerUserLib')
 var express = require("express");
 var router = express.Router();
 
-router.get("/getallrecords",truecallerUserLib.getAllRecords)
-router.get("/getrecordbynumber",truecallerUserLib.getRecordByNumber)
+router.get("/getAllRecords",truecallerUserLib.getAllRecords)
+router.get("/getRecordByNumber",truecallerUserLib.getRecordByNumber)
 router.get('/deleteTruecallerUser',truecallerUserLib.deleteTruecallerUser)
 router.post('/insertTruecallerUser',truecallerUserLib.insertTruecallerUser)
 
