65a6aaa48ff6150b41f54d20aaebb5c91e0315ec
Karthik-184 <patelkarthikreddy@gmail.com>
2022-12-17 16:05:41 +0530

    issues resolved

--- backend/routes/allRoutes.js
+++ backend/routes/allRoutes.js
@@ -1,14 +1,8 @@
-const msg = require('../lib/messageLib')
-const userLib = require('../lib/userlib')
 const truecallerUserLib = require('../lib/truecallerUserLib')
 
 var express = require("express");
 var router = express.Router();
-router.get('/hello', function(req, res){
-    res.send('Hello World');
-});
-router.get('/getMessage',msg.getMessage);
-router.get('/get')
+
 router.get('/deleteTruecallerUser',truecallerUserLib.deleteTruecallerUser)
 router.post('/insertTruecallerUser',truecallerUserLib.insertTruecallerUser)
 
