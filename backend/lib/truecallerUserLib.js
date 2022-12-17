90af3d021d4aa58c60884349ac11695c60c55362
Karthik-184 <patelkarthikreddy@gmail.com>
2022-12-17 09:53:54 +0530

    conflicts resolved

--- backend/lib/truecallerUserLib.js
+++ backend/lib/truecallerUserLib.js
@@ -3,34 +3,34 @@ const asyncDbLib = require('../lib/asyncDbLib')
 const logger = require("../utils/logger").getLogForLib();
 
 // function to insert a record 
-module.exports.insertTruecallerUser = async(req,res) => {
-  try{
+module.exports.insertTruecallerUser = async (req, res) => {
+  try {
     let data = {
-      phone     : req.body.phone,
-      name      : req.body.name,
-      location  : req.body.location,
-      email     : req.body.email
+      phone: req.body.phone,
+      name: req.body.name,
+      location: req.body.location,
+      email: req.body.email
     }
-    const result = await asyncDbLib.createDocument(truecallerUserModel,data)
+    const result = await asyncDbLib.createDocument(truecallerUserModel, data)
     logger.debug(result)
     res.status(200).json(result)
   }
-  catch(err){
+  catch (err) {
     logger.debug(err)
     res.status(500).json(err)
   }
 }
 // function to delete a record
-module.exports.deleteTruecallerUser = async(req,res) => {
-  try{
+module.exports.deleteTruecallerUser = async (req, res) => {
+  try {
     let query = {
-      phone : req.body.phone
+      phone: req.body.phone
     }
-    const result = await asyncDbLib.deleteDocument(truecallerUserModel,query)
+    const result = await asyncDbLib.deleteDocument(truecallerUserModel, query)
     logger.debug(result)
     res.status(200).json(result)
   }
-  catch(err){
+  catch (err) {
     logger.debug(err)
     res.status(500).json(err)
   }
@@ -40,7 +40,7 @@ module.exports.deleteTruecallerUser = async(req,res) => {
 module.exports.getAllRecords = async (req, res) => {
   try {
     const allrecords = await asyncDbLib.getAllDocumentsWithFilter(truecallerUserModel, {})
-    logger.debug("allrecords =",allrecords)
+    logger.debug("allrecords =", allrecords)
     res.status(200).json(allrecords)
   }
   catch (err) {
@@ -54,7 +54,7 @@ module.exports.getRecordByNumber = async (req, res) => {
     let phonenumber = req.query.phone
     let filter = { phone: phonenumber }
     const result = await asyncDbLib.getOneDocumentByFilter(truecallerUserModel, filter);
-    logger.debug("result is ",result)
+    logger.debug("result is ", result)
     res.status(200).json(result)
   }
   catch (err) {
