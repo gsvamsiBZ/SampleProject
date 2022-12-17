39a07593a714cf047fe7bcec378006a0ebfd0de3
Siddhu <siddharthar938@gmail.com>
2022-12-17 09:29:18 +0530

    updated my records

--- backend/lib/truecallerUserLib.js
+++ backend/lib/truecallerUserLib.js
@@ -1,28 +1,29 @@
-// insert function 
-// delete function
-// get all records function
-// get 1 record function
-const truecallermodel=require("../db/models/truecallerUserModel")
-const asyncDblib= require("./asyncDbLib")
-module.exports.getAllRecords=async(req,res)=>{
-    try{
-             const allrecords=await asyncDblib.getAllDocumentsWithFilter(truecallermodel,{})
-             console.log("getting all records ",allrecords)
-             res.json(allrecords)
-    }
-    catch(error){
-        res.status(500).json(error);
-    }
+const logger = require("../utils/logger").getLogForLib();
+const truecallermodel = require("../db/models/truecallerUserModel")
+const asyncDblib = require("./asyncDbLib")
+
+//function to return all records
+module.exports.getAllRecords = async (req, res) => {
+  try {
+    const allrecords = await asyncDblib.getAllDocumentsWithFilter(truecallermodel, {})
+    logger.debug("allrecords =",allrecords)
+    res.status(200).json(allrecords)
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 }
-module.exports.getRecordByNumber=async(req,res)=>{
-    try{
-        let phonenumber=req.query.phone 
-        let filter={phone:phonenumber}
-        const data=await asyncDblib.getOneDocumentByFilter(truecallermodel,filter);
-        console.log("getting records with phone filter of ",filter,"and the data is ",data);
-        res.json(data)
-    }
-    catch(error){
-         res.status(500).json(error);
-    }
+
+//function to return record with phone filter
+module.exports.getRecordByNumber = async (req, res) => {
+  try {
+    let phonenumber = req.query.phone
+    let filter = { phone: phonenumber }
+    const data = await asyncDblib.getOneDocumentByFilter(truecallermodel, filter);
+    logger.debug("data is ",data)
+    res.status(200).json(data)
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 }
