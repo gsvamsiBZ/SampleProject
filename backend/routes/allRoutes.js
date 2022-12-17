8d07f2edbe3705e5c3edb87b8dd55f6b6228527f
Karthik-184 <patelkarthikreddy@gmail.com>
2022-12-17 13:51:41 +0530

    added insertion and deletion routes

--- backend/routes/allRoutes.js
+++ backend/routes/allRoutes.js
@@ -1,6 +1,15 @@
+const msg = require('../lib/messageLib')
+const userLib = require('../lib/userlib')
+const truecallerUserLib = require('../lib/truecallerUserLib')
+
 var express = require("express");
 var router = express.Router();
-router.get('/hello', function(req, res) {
+router.get('/hello', function(req, res){
     res.send('Hello World');
 });
+router.get('/getMessage',msg.getMessage);
+router.get('/get')
+router.get('/deleteUser',truecallerUserLib.deleteTruecallerUser)
+router.post('/insertUser',truecallerUserLib.insertTruecallerUser)
+
 module.exports = router;
