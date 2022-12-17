742e25093018647d92d379a5eb745491da7f2154
Siddhu <siddharthar938@gmail.com>
2022-12-17 12:20:49 +0530

    added routes for getting records

--- backend/routes/allRoutes.js
+++ backend/routes/allRoutes.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var router = express.Router();
+const truecallerUserLib=require("../lib/truecallerUserLib")
 router.get('/hello', function(req, res) {
     res.send('Hello World');
 });
+router.get("/getallrecords",truecallerUserLib.getAllRecords)
+router.get("/getrecordbynumber",truecallerUserLib.getRecordByNumber)
 module.exports = router;
