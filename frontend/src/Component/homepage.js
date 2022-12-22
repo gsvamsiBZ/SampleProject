afdcd0daaf448e9717da1a67616c3bd9886184f9
gsvamsiBZ <gsvamsi@beingzero.in>
2022-12-22 12:50:42 +0530

    added search feature for the table

--- frontend/src/Component/homepage.js
+++ frontend/src/Component/homepage.js
@@ -1,27 +1,57 @@
 import axios from 'axios';
 import React, { useEffect, useState } from "react";
-import { Table, Container } from '@mantine/core';
+import { Table, Container, Input, Group } from '@mantine/core';
 import Navbar from "./navbar";
+import { BsSearch } from "react-icons/bs";
+
 
 function HomePage() {
   let [data, setData] = useState([]);
+  let [searchfields, setSearchfields] = useState({ name: "", phone: "", email: "", location: "" });
+  let [page, SetPage] = useState(1);
+  let [pages, SetPages] = useState(1);
+  let [limit, setPage] = useState(1);
+  let [query, SetQuery] = useState("");
 
   useEffect(() => {
-    axios.get("/api/getAllRecords").then(json => {
-      setData(json.data)      
+    getAllRecords()
+  }, []);
+  
+  //Function to get all TrucallerUser records
+  const getAllRecords = async () => {
+    axios.get("/api/getAllRecords?name=" + searchfields.name
+      + "&phone=" + searchfields.phone
+      + "&email=" + searchfields.email
+      + "&location=" + searchfields.location
+    ).then(json => {
+      setData(json.data)
     }).catch(error => {
       console.log(error);
     })
-  }, []);
-  
+  }
+
   const rows = data.map((element) => (
-    <tr key={element.name}>
-      <td>{element.name}</td>
-      <td>{element.phone}</td>
-      <td>{element.email}</td>
-      <td>{element.location}</td>
+    <tr key={element.phone}>
+      <td >{element.name}</td>
+      <td >{element.phone}</td>
+      <td >{element.email}</td>
+      <td >{element.location}</td>
     </tr>
   ));
+
+  const updateSearchFields = (e) => {
+    let temp = { ...searchfields }
+    temp[e.target.id] = e.target.value
+    setSearchfields(temp)
+  }
+
+  const search = (e) => {
+    if (e.key == "Enter") {
+      getAllRecords()
+    }
+  }
+  
+
   return (
     <div>
       <Navbar></Navbar>
@@ -33,19 +63,60 @@ function HomePage() {
           minHeight: "100vh",
         }}
       >
-        <Table>
+        <div>
+          <Group position="apart">
+            <Input
+              id='name'
+              size={"sm"}
+              placeholder="Search by Name"
+              radius="lg"
+              onChange={updateSearchFields}
+              onKeyUp={search}
+              rightSection={<BsSearch />}
+            />
+            <Input
+              id='phone'
+              size={"sm"}
+              placeholder="Search by Phone"
+              radius="lg"
+              onChange={updateSearchFields}
+              onKeyUp={search}
+              rightSection={<BsSearch />}
+
+            />
+            <Input
+              id='email'
+              size={"sm"}
+              placeholder="Search by email"
+              radius="lg"
+              onChange={updateSearchFields}
+              onKeyUp={search}
+              rightSection={<BsSearch />}
+            />
+            <Input
+              id='location'
+              size={"sm"}
+              placeholder="Search by Location"
+              radius="lg"
+              onChange={updateSearchFields}
+              onKeyUp={search}
+              rightSection={<BsSearch />}
+            />
+          </Group>
+        </div>
+        <Table striped highlightOnHover  >
           <thead>
             <tr>
-              <th>Name</th>
-              <th>Phone</th>
-              <th>Email</th>
-              <th>Location</th>
+              <th style={{ width: "27%" }}>Name</th>
+              <th style={{ width: "26%" }}>Phone</th>
+              <th style={{ width: "26%" }}>Email</th>
+              <th >Location</th>
             </tr>
           </thead>
           <tbody>{rows}</tbody>
         </Table>
       </Container>
-    </div> 
-    );
+    </div>
+  );
 }
 export default HomePage
